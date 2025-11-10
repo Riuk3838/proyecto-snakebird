456048ab4693c0c02b6b8cc5f66c337360e40c43
Rui Paolo Antunes Faisal <edomex215@amerike.edu.mx>
2025-11-10 11:21:49 -0600

    terrain

---
+++ main.c
@@ -0,0 +1,89 @@
+
+
+#include "raylib.h"
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//structura
+typedef struct square_collider{ 
+    Vector2 position;
+    int size;
+} square_collider;
+
+int collision (Vector2 point, square_collider col);
+
+square_collider terrain[10]; 
+
+//----------------------------------------------------------------------------------
+// Local Functions Declaration
+//----------------------------------------------------------------------------------
+static void UpdateDrawFrame(void);          // Update and draw one frame
+
+//----------------------------------------------------------------------------------
+// Main entry point
+//----------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "snakebird");
+    terrain = {
+        
+    }
+    
+    
+    //--------------------------------------------------------------------------------------
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();                  // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+// Update and draw game frame
+static void UpdateDrawFrame(void)
+{
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(SKYBLUE);
+
+        for (int i = 0; i < 10; i++)
+        {
+            DrawRectangle(terrain[i].position.x, terrain[i].position.y, terrain[i].size, terrain[i].size, GREEN);
+        }
+    
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
+
+int collision (Vector2 point, square_collider col)
+{
+    if(point.x>col.position.x-20 && point.x<col.position.x+20 && point.y>col.position.y-20 && point.y<col.position.y+20)
+    {
+        return 1;
+    }
+    return 0;
+}
